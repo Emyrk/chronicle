c20e07f7460996d464097138efe6cf7d7a15336c
Steven Masley <stevenmasley@gmail.com>
2026-08-13 22:26:28 +0000

    fix(combatlog/parser/wotlk/instances): add missing comma in Naxxramas ZoneNames

--- combatlog/parser/wotlk/instances/raids.go
+++ combatlog/parser/wotlk/instances/raids.go
@@ -202,12 +202,14 @@ func NaxxramasSpeedrunRequirements() []rankings.SpeedrunRequirement {
 }
 
 var NaxxramasFactory = &instances.CommonFactory{
-	Name:      "Naxxramas",
-	ZoneNames: []string{"naxxramas", "the upper necropolis"
-"上层大墓地", // Upper Necropolis
-},
-	MapIDs:    []uint32{533},
-	Hostiles:  NaxxramasHostiles,
+	Name: "Naxxramas",
+	ZoneNames: []string{
+		"naxxramas",
+		"the upper necropolis",
+		"上层大墓地", // Upper Necropolis
+	},
+	MapIDs:   []uint32{533},
+	Hostiles: NaxxramasHostiles,
 	FlavoredRankings: func(database.WoWFlavor) *rankings.Rankings {
 		return &rankings.Rankings{
 			Speedrun: &rankings.SpeedrunRules{
