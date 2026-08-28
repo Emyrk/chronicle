bec091e1d442721b43f68293c92f5e27652a27c7
Steven Masley <stevenmasley@gmail.com>
2026-08-28 17:19:16 -0500

    fix: track sunder stacks from devastate

--- frontend/chronicle/src/pages/Instance/EventsPanels/Sunder/Sunder.explainer.tsx
+++ frontend/chronicle/src/pages/Instance/EventsPanels/Sunder/Sunder.explainer.tsx
@@ -6,7 +6,8 @@ import type { PanelExplainer } from "../../PanelExplainer/types";
 
 export const sunderExplainer: PanelExplainer = {
   summary:
-    "Tracks Sunder Armor effectiveness by warriors. A sunder is 'effective' if it actually applies a stack to the target. " +
+    "Tracks Sunder Armor effectiveness by warriors, including stacks applied by Devastate and Glyph of Devastate. " +
+    "A sunder is 'effective' if it actually applies a stack to the target. " +
     "If the target dodges, parries, or is already at 5 stacks, the sunder is wasted. " +
     "Use this to evaluate warrior coordination and time-to-5-stacks on bosses.",
 
