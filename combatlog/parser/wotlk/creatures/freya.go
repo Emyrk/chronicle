0f3048d2dc1f29899bac9fa8bcda6777c033262c
Steven Masley <stevenmasley@gmail.com>
2026-09-02 20:44:48 -0500

    freya work

--- combatlog/parser/wotlk/creatures/freya.go
+++ combatlog/parser/wotlk/creatures/freya.go
@@ -1,6 +1,8 @@
 package creatures
 
 import (
+	"time"
+
 	"github.com/Emyrk/chronicle/combatlog/parser/common/characters"
 	"github.com/Emyrk/chronicle/combatlog/parser/common/messages"
 	"github.com/Emyrk/chronicle/combatlog/parser/guid"
@@ -19,8 +21,11 @@ func NewFreyaEncounterCharacter(id guid.GUID, all *characters.Characters) (chara
 		return nil, false
 	}
 
+	c := characters.NewCommonCharacter(id, all)
+	c.SetRecentlySlainDuration(time.Second * 30)
+
 	return &freyaCharacter{
-		Common: characters.NewCommonCharacter(id, all),
+		Common: c,
 		defeat: characters.NewScriptedDefeatDetector(id, freyaDefeatConfig()),
 	}, true
 }
