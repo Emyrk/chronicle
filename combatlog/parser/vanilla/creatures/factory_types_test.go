28b7829d1b53766dbe6beebd290d9b6d830f790c
Steven Masley <stevenmasley@gmail.com>
2026-09-06 18:37:29 -0500

    fix: persist unrecognized pets

--- combatlog/parser/vanilla/creatures/factory_types_test.go
+++ combatlog/parser/vanilla/creatures/factory_types_test.go
@@ -2,11 +2,14 @@ package creatures
 
 import (
 	"testing"
+	"time"
 
 	"github.com/Emyrk/chronicle/combatlog/parser/common/characters"
 	"github.com/Emyrk/chronicle/combatlog/parser/common/identifier"
+	"github.com/Emyrk/chronicle/combatlog/parser/common/messages"
 	"github.com/Emyrk/chronicle/combatlog/parser/common/unitdb"
 	"github.com/Emyrk/chronicle/combatlog/parser/guid"
+	"github.com/Emyrk/chronicle/combatlog/parser/types/unitinfo"
 	"github.com/Emyrk/chronicle/database"
 	"github.com/stretchr/testify/require"
 )
@@ -15,28 +18,72 @@ func creatureEntryGUID(high uint64, entry uint32) guid.GUID {
 	return guid.GUID(high | uint64(entry)<<24 | 1)
 }
 
-func factoryMatch(factories []characters.CharacterFactory, id guid.GUID, chars *characters.Characters) bool {
-	for _, factory := range factories {
-		if _, ok := factory(id, chars); ok {
-			return true
-		}
-	}
-	return false
-}
-
-func TestVanillaEncounterFactoriesRejectPetEntries(t *testing.T) {
+func TestUnrecognizedPetsPersistWithoutActivity(t *testing.T) {
 	t.Parallel()
 
+	factories := VanillaCharacterFactories(database.WoWFlavor{database.FlavorVanilla})
+	db := unitdb.New()
 	chars := characters.NewCharacters(
-		unitdb.New(),
-		nil,
+		db,
+		factories,
 		identifier.NewIdentifier(map[uint32]identifier.Identity{}),
 	)
-	factories := VanillaCharacterFactories(database.WoWFlavor{database.FlavorVanilla})
 
-	const nefarianEntry = 11583
-	require.False(t, factoryMatch(factories, creatureEntryGUID(0xF140000000000000, nefarianEntry), chars))
-	require.True(t, factoryMatch(factories, creatureEntryGUID(0xF130000000000000, nefarianEntry), chars))
+	petID := guid.GUID(0xF1400234DC000005)
+	ownerID := guid.GUID(0x000000003B9BC56F)
+	now := time.UnixMilli(1788657573465)
+	db.Update(unitinfo.Info{
+		Seen:         now,
+		Guid:         petID,
+		Name:         "Sproutling",
+		CanCooperate: true,
+		Owner:        &ownerID,
+	})
+
+	char, added := chars.Add(petID, now)
+	require.True(t, added)
+	pet, ok := char.(characters.InstanceUnitPersister)
+	require.True(t, ok)
+	require.True(t, pet.PersistInInstance())
+	require.False(t, char.IsActive())
+
+	_, err := chars.Process(&messages.Heal{
+		MessageBase: messages.Base(now),
+		Caster:      petID,
+		Target:      ownerID,
+		SpellName:   "Herbal Mend",
+	})
+	require.NoError(t, err)
+	require.False(t, char.IsActive())
+
+	info, ok := db.Get(petID)
+	require.True(t, ok)
+	require.Equal(t, "Sproutling", info.Name)
+	require.Equal(t, ownerID, *info.Owner)
+}
+
+func TestUnrecognizedPetFallbackRunsAfterSpecializedFactories(t *testing.T) {
+	t.Parallel()
+
+	petID := guid.GUID(0xF140000001000001)
+	factories := []characters.CharacterFactory{
+		func(id guid.GUID, all *characters.Characters) (characters.Character, bool) {
+			if id != petID {
+				return nil, false
+			}
+			return characters.NewCommonCharacter(id, all), true
+		},
+		NewUnrecognizedPet,
+	}
+	chars := characters.NewCharacters(
+		unitdb.New(),
+		factories,
+		identifier.NewIdentifier(map[uint32]identifier.Identity{}),
+	)
+
+	char, added := chars.Add(petID, time.Time{})
+	require.True(t, added)
+	require.IsType(t, &characters.Common{}, char)
 }
 
 func TestNewObjectRequiresObjectGUID(t *testing.T) {
